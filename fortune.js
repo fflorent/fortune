0bc83ad6eb00337e208f948bd8c9fc917490914a
Florent FAYOLLE <cold_sun@hotmail.fr>
2014-07-04 16:50:17 +0200

    Issue #4: Allow multi-line fortunes

--- fortune.js
+++ fortune.js
@@ -20,7 +20,55 @@
     function displayRandomFortune()
     {
         var randomIndex = Math.floor(Math.random() * fortunes.length);
-        fortuneParagraph.textContent = fortunes[randomIndex];
+        var docFrag = document.createDocumentFragment();
+        var splitFortune = fortunes[randomIndex].replace(/^"|"$/g, "").split(/\n/);
+        splitFortune.forEach(function(text, index)
+        {
+            if (index > 0)
+                docFrag.appendChild(document.createElement("br"));
+
+            docFrag.appendChild(document.createTextNode(text));
+        });
+
+        while (fortuneParagraph.hasChildNodes())
+            fortuneParagraph.firstChild.remove();
+
+        fortuneParagraph.appendChild(docFrag);
+    }
+
+    /**
+     * Parse the fortunes according to the following rules:
+     * - The comments (lines beginning with a "#") are ignored
+     * - Allow multiline quotes:
+     *    * the text begins with a quote (")
+     *    * the text ends with a quote
+     *    * if a quote is in the text, wait for the closing quote
+     *
+     * @param {string} fortunesText The raw text of the fortunes (downloaded through XHR).
+     *
+     * @return {array} The splited and parsed fortunes.
+     */
+    function parseFortunes(fortunesText)
+    {
+        var fortunes = fortunesText.split("\n");
+        fortunes = fortunes.reduce(function(array, text, index)
+        {
+            var lastItem = array[array.length - 1];
+
+            if (lastItem && lastItem.startsWith('"') && !(lastItem.endsWith('"') &&
+                lastItem.match('"', "g").length % 2 === 0))
+            {
+                array.splice(array.length - 1, 1, lastItem + "\n" + text);
+                return array;
+            }
+            // Only keep non-empty and non-commented lines.
+            if (text && !text.startsWith("#"))
+                array.push(text);
+
+            return array;
+        }, []);
+
+        return fortunes;
     }
 
     function loadFortunes()
@@ -44,12 +92,9 @@
                         setErrorMessage("Could not get the fortunes: no text retrieved");
                         return;
                     }
-                    fortunes = fortunesText.split("\n");
-                    fortunes = fortunes.filter(function(text)
-                    {
-                        // Only keep non-empty and non-commented lines.
-                        return text && !text.startsWith("#");
-                    });
+                    // Let's parse the fortunes.
+                    fortunes = parseFortunes(fortunesText);
+                    // Display the fortunes.
                     document.body.classList.remove("state-loading");
                     displayRandomFortune();
 
